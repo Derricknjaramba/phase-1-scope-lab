b0b6efcab8953dbce811b25009fb2e93bd5bb34b
Derricknjaramba <njarambaderrick@gmail.com>
2024-07-07 11:10:15 +0300

    scopelab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+function upperCaseCustomerName() {
+    console.log (customerName.toUpperCase());
+  }
+  
+  console.log(upperCaseCustomerName());
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  setBestCustomer();
+
+  console.log(bestCustomer);
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  
+  overwriteBestCustomer();
+  console.log(bestCustomer); 
+  const leastFavoriteCustomer = 'Alice';
+  const changeleastFavoriteCustomer = 'Alice';
+
+  function changeLeastFavoriteCustomer() {
+  
+    leastFavoriteCustomer = 'Bob';
+  }
